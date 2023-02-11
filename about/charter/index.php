7a7bae8b69ceef48b7481d9fe653738f49f4f9de
Epsilon Community <contact@epsilon.community>
2023-02-11 14:58:44 +0000

    linked to google docs

--- about/charter/index.php
+++ about/charter/index.php
@@ -16,38 +16,8 @@ require_once('../../assets/template/header.php');
 	<h6>To provide a safe and friendly online space for gaming enthusiasts to connect, learn, grow, and socialize.</h6>
 	<hr>
 	<p>
-	<h2>Purpose</h2>
-	To provide a safe and friendly online space for gaming enthusiasts to connect, learn, grow, and socialize.
-	<h2>Values</h2>
-	A positive and respectful community.
-	Open to everyone.
-	Promotion of teamwork, cooperation, and inclusiveness.
-	<h2>Code of Conduct</h2>
-	Members must abide by the code of conduct, including but not limited to:
-	No hate speech, bullying, discrimination, or harassment.
-	Respect for privacy.
-	Members are also bound by other code of conduct such as:
-	Discord's Community Guidelines.
-	Squad's Code of Conduct.
-	Moderation:
-	The community will be moderated by appointed members.
-	Moderators have the authority to remove content violating code of conduct and ban violators.
-	<h2>Learning and Growth</h2>
-	Members are encouraged to learn new skills and improve abilities.
-	Sharing tips and advice with each other.
-	<h2>Socializing and Friendship</h2>
-	A place for members to form new relationships.
-	Group chats and online gaming sessions.
-	<h2>Communication</h2>
-	Members can communicate through the community platform, voice chats, and text chats.
-	Sharing experiences and feedback encouraged.
-	<h2>Privacy</h2>
-	Personal information will not be shared with third parties.
-	Members are encouraged to protect their privacy online.
-	<h2>Agreement</h2>
-	By joining Epsilon Community, members agree to abide by this charter and the code of conduct.
-	The charter may be updated and revised periodically to reflect changing needs.
-	<em>Note: The above is subject to change and may be updated to align with the evolving nature of the community.</em>
+		<a href="https://docs.google.com/document/d/1EmWGFEXwxqu4dBduRlxZagVGpohgmkPIPg3tO7lEhO4/edit?usp=sharing">Epsilon
+			Community Charter Documentation (Google Docs)</a>
 	</p>
 	<br>
 	<br>
