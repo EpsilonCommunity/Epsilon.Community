d1b787f17b74c9d0daca16e0b9096affa58a7f8f
Epsilon Community <contact@epsilon.community>
2023-03-20 14:58:47 +0000

    Year is always current year.

--- assets/template/footer.php
+++ assets/template/footer.php
@@ -1,6 +1,8 @@
 <div class="container text-white">
   <div class="d-flex justify-content-between align-items-center py-4 my-4 border-top">
-    <p class="align-items-center">Epsilon Community &copy; 2018-2023</p>
+    <p class="align-items-center">Epsilon Community &copy; 2018-
+      <?= date("Y") ?>
+    </p>
     <a class="navbar-brand align-items-center" href="https://epsilon.community"><img
         src="https://epsilon.community/assets/images/brand/epsiloncommunity.svg" width="40" height="40"></a>
     <ul class="align-items-center list-unstyled d-flex">
