d7947c7bef12f4970a957892cacaed2bb2c3bf27
Epsilon Community <contact@epsilon.community>
2023-03-20 15:00:18 +0000

    Update footer.php

--- assets/template/footer.php
+++ assets/template/footer.php
@@ -1,7 +1,7 @@
 <div class="container text-white">
   <div class="d-flex justify-content-between align-items-center py-4 my-4 border-top">
     <p class="align-items-center">Epsilon Community &copy; 2018-
-      <?= date("Y") ?>
+      <?= date("Y"); ?>
     </p>
     <a class="navbar-brand align-items-center" href="https://epsilon.community"><img
         src="https://epsilon.community/assets/images/brand/epsiloncommunity.svg" width="40" height="40"></a>
